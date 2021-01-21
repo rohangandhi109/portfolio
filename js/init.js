b337399754f2803c699144ee0ddd68d2807dd2b9
Rohan Gandhi <rohangandhi109@gmail.com>
2021-01-20 19:02:59 -0500

    Updates

--- js/init.js
+++ js/init.js
@@ -149,30 +149,34 @@
       var data = 'contactName=' + contactName + '&contactEmail=' + contactEmail +
                '&contactSubject=' + contactSubject + '&contactMessage=' + contactMessage;
 
-      $.ajax({
+      // $.ajax({
 
-	      type: "POST",
-	      url: "inc/sendEmail.php",
-	      data: data,
-	      success: function(msg) {
+	   //    type: "POST",
+	   //    url: "inc/sendEmail.php",
+	   //    data: data,
+	   //    success: function(msg) {
 
-            // Message was sent
-            if (msg == 'OK') {
-               $('#image-loader').fadeOut();
-               $('#message-warning').hide();
-               $('#contactForm').fadeOut();
-               $('#message-success').fadeIn();   
-            }
-            // There was an error
-            else {
-               $('#image-loader').fadeOut();
-               $('#message-warning').html(msg);
-	            $('#message-warning').fadeIn();
-            }
+      //       // Message was sent
+      //       if (msg == 'OK') {
+      //          $('#image-loader').fadeOut();
+      //          $('#message-warning').hide();
+      //          $('#contactForm').fadeOut();
+      //          $('#message-success').fadeIn();   
+      //       }
+      //       // There was an error
+      //       else {
+      //          $('#image-loader').fadeOut();
+      //          $('#message-warning').html(msg);
+	   //          $('#message-warning').fadeIn();
+      //       }
 
-	      }
+	   //    }
 
-      });
+      // });
+      $('#image-loader').fadeOut();
+      $('#message-warning').hide();
+      $('#contactForm').fadeOut();
+      $('#message-success').fadeIn();  
       return false;
    });
 
